390e8c3dcb6b4df3eb13af78fc58ace85e7aa2f6
iam-vihaan <asathyavihaan@gmail.com>
2025-06-05 11:10:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
-
+terraform {
   backend "s3" {
-    bucket = "demo-usecases-bucket-new"
-    key    = "usecase-02/terraform.tftstate"
-    region = "us-east-1"
+    bucket = "your-bucket-name"
+    key    = "path/to/your/terraform.tfstate"
+    region = "your-region"
   }
+}
