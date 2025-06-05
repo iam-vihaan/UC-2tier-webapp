a1446a8d51493d7eeeb9ddf7d3bb5181e93851d3
iam-vihaan <asathyavihaan@gmail.com>
2025-06-05 09:36:04 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,61 +1,79 @@
-
 variable "region" {
-  description = "The AWS region to deploy resources"
-  default = "us-esat-1"
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default = "10.0.0.0/16"
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "public_subnets_cidr" {
-  description = "CIDR blocks for the public subnets"
-  type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_subnets_cidr" {
-  description = "CIDR blocks for the private subnets"
-  type = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 variable "availability_zones" {
-  description = "Availability zones for the subnets"
-  type = list(string)
-  default = ["us-west-2a", "us-west-2b"]
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "environment" {
+  description = "The Name of the environment"
+  type        = string
+  default     = "Dev"
+}
+
+variable "key_name" {
+  description = "The key name of the instance"
+  type        = string
+  default     = "devops-tools"
 }
 
 variable "ami_id" {
-  description = "AMI ID for the web server instances"
-  default = "ami-0c55b159cbfafe1f0"
+  description = "AMI ID for the EC2 instances"
+  type        = string
+  default     = "ami-084568db4383264d4"
 }
 
 variable "instance_type" {
-  description = "Instance type for the web server instances"
-  default = "t2.micro"
+  description = "Instance type for the EC2 instances"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "db_name" {
-  description = "Database name"
-  default = "mydb"
+  description = "The name of the database"
+  type        = string
+  default     = "mydatabase"
 }
 
 variable "db_username" {
-  description = "Database username"
-  default = "admin"
+  description = "The username for the database"
+  type        = string
+  default     = "admin"
 }
 
 variable "db_password" {
-  description = "Database password"
-  default = "password"
+  description = "The password for the database"
+  type        = string
+  default     = "mypassword"
 }
 
-variable "s3_bucket" {
-  description = "S3 bucket for remote state"
-}
-
-variable "dynamodb_table" {
-  description = "DynamoDB table for state locking"
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default = {
+    Name = "TerraformProject"
+  }
 }
