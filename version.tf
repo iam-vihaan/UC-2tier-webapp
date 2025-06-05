e14a7932e7db564e30e2bf4ec5ca607213fdd87b
iam-vihaan <asathyavihaan@gmail.com>
2025-06-05 09:36:31 +0530

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "demo-usecases-bucket-new"
+    key    = "usecase-02/terraform.tftstate"
+    region = "us-east-1"
+  }
+}
